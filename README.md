10483ecd9de8df630572dd7018e91758cdfe9685
Quarterican-KJ <38819981+Quarterican-KJ@users.noreply.github.com>
2018-09-16 17:59:09 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Restaurant-Review-App
 This is a web app that displays various restaurant reviews.
-still needs aria roles, not sure the abest rols for various values.
--- js/main.js
+++ js/main.js
@@ -174,7 +174,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
-  image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt "Image of the Resaurant" image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h1');
